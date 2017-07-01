95151ac75718cc865707fe21b3bc97bf30c21b3c
Iglói Gábor <gaborigloi@gmail.com>
2017-07-01 11:43:20 +0100

    Travis: document steps in test script

--- test.sh
+++ test.sh
@@ -1,10 +1,15 @@
 set -uex
 
+# Extract the regular expression from the format file
 REGEXP=$(jq .xensource_log.regex.xensource_log.pattern xensource.json | sed 's/\\\\/\\/g' | sed 's/^"\(.*\)"$/\1/')
 
+# Test that the regexp is correct and matches at least one line in the test log
 pcregrep --quiet --regexp="$REGEXP" test/test_match/xensource.log
+# Test that the regexp matches every line in the test log
 (! pcregrep --regexp="$REGEXP" --invert-match test/test_match/xensource.log)
 
+# For each capture group, extract it from every line, and compare the output to
+# the expected result
 for group in $(cat test/test_captures/capturing_groups); do
   perl -pe "s/$REGEXP/\$+{$group}/" < test/test_captures/xensource.log > /tmp/output
   diff test/test_captures/expected_matches/$group /tmp/output
