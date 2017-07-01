8a58a151146553d86c4e6f3ba7457edef22c4d25
Iglói Gábor <gaborigloi@gmail.com>
2017-07-01 11:37:09 +0100

    Travis: document steps in test script

--- test.sh
+++ test.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 set -uex
 
+# This branch and test is for the 0.8.X versions of lnav
 [[ "$(lnav -V)" == lnav\ 0.8* ]]
+
+# If we have the latest lnav version, check that there are no warnings
 test "$(lnav -V)" == "lnav 0.8.2" && \
   (! (lnav-0.8.2/lnav |& grep "^warning:"))
+
 lnav -C
