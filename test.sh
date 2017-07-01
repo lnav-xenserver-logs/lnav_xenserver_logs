8e36dbc9db3596b271cf71b66d9cfaf782eaddb0
Iglói Gábor <gaborigloi@gmail.com>
2017-07-01 12:18:27 +0100

    Travis: test the individual fields
    Check that they capture the expected strings.

--- test.sh
+++ test.sh
@@ -9,3 +9,7 @@ test "$(lnav -V)" == "lnav 0.8.2" && \
   (! (lnav-0.8.2/lnav |& grep "^warning:"))
 
 lnav -C
+
+# Test that the different fields are extracted correctly
+lnav -qn -c ';select xapi_timestamp,level,xapi_hostname,threadid,origin,task,module from xensource_log_module_format' -c ':write-csv-to /tmp/out.csv' ./test/test_captures/xensource.log
+diff /tmp/out.csv test/test_captures/expected_matches.csv
