a3fe2c90a8ae12a421e4241ba22feac8acbf4c92
Gabor Igloi <gabor.igloi@citrix.com>
2017-07-11 16:51:31 +0100

    Also test & support older Cream XS release
    with slightly different log format.

--- test.sh
+++ test.sh
@@ -1,16 +1,25 @@
+#!/bin/bash
+
 set -uex
 
 # Extract the regular expression from the format file
 REGEXP=$(jq .xensource_log.regex.xensource_log.pattern xensource.json | sed 's/\\\\/\\/g' | sed 's/^"\(.*\)"$/\1/')
 
-# Test that the regexp is correct and matches at least one line in the test log
-pcregrep --quiet --regexp="$REGEXP" test/test_match/xensource.log
-# Test that the regexp matches every line in the test log
-(! pcregrep --regexp="$REGEXP" --invert-match test/test_match/xensource.log)
+cd test
+for release in xs_*; do
+  pushd "$release"
 
-# For each capture group, extract it from every line, and compare the output to
-# the expected result
-for group in $(cat test/test_captures/capturing_groups); do
-  perl -pe "s/$REGEXP/\$+{$group}/" < test/test_captures/xensource.log > /tmp/output
-  diff test/test_captures/expected_matches/$group /tmp/output
+  # Test that the regexp is correct and matches at least one line in the test log
+  pcregrep --quiet --regexp="$REGEXP" test_match/xensource.log
+  # Test that the regexp matches every line in the test log
+  (! pcregrep --regexp="$REGEXP" --invert-match test_match/xensource.log)
+
+  # For each capture group, extract it from every line, and compare the output to
+  # the expected result
+  for group in $(cat ../capturing_groups); do
+    perl -pe "s/$REGEXP/\$+{$group}/" < test_captures/xensource.log > /tmp/output
+    diff test_captures/expected_matches/$group /tmp/output
+  done
+
+  popd
 done
